a3dfe31fa98e8e9b5f6a70810ff4889862ef8fec
WIN-S555EIEU7KL\Administrator <send2dendy@gmail.com>
2023-03-14 14:05:46 +0700

    ScriptableObject StatusPlate

---
+++ Assets/Scripts/StatusPlate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Status", menuName = "ScriptableObjects/Status Plates", order = 1)]
+public class StatusPlate : ScriptableObject
+{
+    public float health;
+    public float pdamage;
+    public float mdamage;
+    public float armour;
+    public float marmour;
+
+    public bool magtype;
+}
